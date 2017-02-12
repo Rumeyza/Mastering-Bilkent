b23ef8dc3d33e1314f38c33fd7bf695fefba65b5
Asus <Asus@AsusPC>
2017-02-12 18:24:56 +0300

    try

--- MasteringBilkent/src/firstclass.java
+++ MasteringBilkent/src/firstclass.java
@@ -3,5 +3,6 @@ public class firstclass {
 	public static void main(String[] args){
 		System.out.println("Hello MasteringBilkent!!");
 		System.out.println("2nd string");
+		System.out.println("arif");
 	}
 }
