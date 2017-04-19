34c4e11ecce3498ea13f4714f8ce8f516b92c3f3
Furkan Arif Bozdağ <bozdag.arif@gmail.com>
2017-04-19 18:50:51 +0300

    Update Main.java

--- MasteringBilkent/src/Main.java
+++ MasteringBilkent/src/Main.java
@@ -17,8 +17,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -40,10 +40,28 @@ public class Main extends Application{
         centerMenu.setSpacing(8);
         centerMenu.setAlignment(Pos.TOP_CENTER);
 
-        Text text1 = new Text(10, 20, "Mastering Bilkent Student Home Page");
+        //MASTERING BILKENT TITLE
+
+        String fontFamily = "Helvetica";
+        double titleFontSize = 48;
+
+        Text scenetitle1 = new Text("MASTERING");
+        scenetitle1.setFill(Color.BLUE);
+        scenetitle1.setFont(Font.font(fontFamily, FontWeight.EXTRA_BOLD, titleFontSize));
+
+        Text scenetitle2 = new Text(" BILKENT");
+        scenetitle2.setFill(Color.RED);
+        scenetitle2.setFont(Font.font(fontFamily, FontWeight.EXTRA_BOLD, titleFontSize));
+
+        HBox logo = new HBox();
+        logo.setPadding(new Insets(10));
+        logo.setSpacing(8);
+        logo.setAlignment(Pos.TOP_CENTER);
+        logo.getChildren().addAll(scenetitle1, scenetitle2);
 
         Hyperlink buttonA = new Hyperlink("Register Another Course");
-        centerMenu.getChildren().addAll(text1, buttonA);
+
+        centerMenu.getChildren().addAll(logo, buttonA);
 
         //Left
         VBox leftMenu = new VBox();
@@ -62,19 +80,19 @@ public class Main extends Application{
         iv1.setFitHeight(100);
         iv1.setImage(avatar);
         GridPane.setConstraints(iv1, 0,0,1,3);
-        
+
         Text text2 = new Text(10, 20, "Furkan Arif Bozdağ");
         GridPane.setConstraints(text2, 1,0,2,1);
-        
+
         Text text3 = new Text(10, 20, "Bilkent University");
         GridPane.setConstraints(text3, 1,1,2,1);
-        
+
         Text text4 = new Text(10, 20, "CS /");
         GridPane.setConstraints(text4, 1,2);
-        
+
         Text text5 = new Text(10, 20, "Undergraduate");
         GridPane.setConstraints(text5, 2,2);
-        
+
         Text text6 = new Text(10, 20, "arif.bozdag@ug.bilkent.edu.tr");
         GridPane.setConstraints(text6, 0,3,3,3);
 
@@ -84,18 +102,24 @@ public class Main extends Application{
 
         Hyperlink link1 = new Hyperlink("Main Page");
         //link1.setOnAction(e -> );
-        
+
         Hyperlink link2 = new Hyperlink("My Courses");
         //link2.setOnAction(e -> );
-        
+
         Hyperlink link3 = new Hyperlink("Profile");
         link3.setOnAction(e -> primaryStage.setScene(scene2));
-        
+
         Hyperlink link4 = new Hyperlink("Send Feedback");
         link4.setOnAction(e -> AlertBox.display("contact","mastering@bilkent.edu.tr"));
-        
+
         Hyperlink link5 = new Hyperlink("Settings");
         //link5.setOnAction(e -> );
+
+
+        Text buttonE = new Text(10, 20,"My Courses");
+        Text buttonF = new Text(10, 20,"Profile");
+        Text buttonG = new Text(10, 20,"Send Feedback");
+        Text buttonH = new Text(10, 20,"Settings");
 
         //search bar
         final TextField searchfield = new TextField();
@@ -109,6 +133,7 @@ public class Main extends Application{
         BorderPane layout1 = new BorderPane();
         layout1.setCenter(centerMenu);
         layout1.setLeft(leftMenu);
+        layout1.setStyle("-fx-background: #FFFFFF;");
 
         //SCENE 1 -> HOME PAGE
         scene1 = new Scene(layout1);
