6de419393cdad31af9cc7509226d427c46d99304
Furkan Arif Bozdağ <bozdag.arif@gmail.com>
2017-04-20 01:11:01 +0300

    Delete firstclass.java

--- MasteringBilkent/src/firstclass.java
+++
@@ -1,8 +0,0 @@
-
-public class firstclass {
-	public static void main(String[] args){
-		System.out.println("Hello MasteringBilkent!!");
-		System.out.println("2nd string");
-		System.out.println("arif");
-	}
-}
