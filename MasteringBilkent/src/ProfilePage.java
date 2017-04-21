810106a28d14b77173418f7fb8c9c568ba526717
Furkan Arif Bozdağ <bozdag.arif@gmail.com>
2017-04-22 02:22:24 +0300

    çılgın gelişmeler
    user class bağlantısı

--- MasteringBilkent/src/ProfilePage.java
+++ MasteringBilkent/src/ProfilePage.java
@@ -1,3 +1,4 @@
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -5,6 +6,7 @@ import javafx.scene.control.Hyperlink;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
@@ -14,54 +16,55 @@ public class ProfilePage {
     static Scene scene;
 
     public static void start() {
+
+        VBox layout = new VBox();
+        layout.setAlignment(Pos.CENTER);
+
         GridPane infogrid = new GridPane();
+        infogrid.setAlignment(Pos.CENTER);
         infogrid.setPadding(new Insets(10,10,10,10));
         infogrid.setVgap(8);
         infogrid.setHgap(10);
 
         //profile image
-        Image avatar = new Image("file:avatar.png");
+        Image avatar = new Image("file:avatar2.jpg");
         ImageView iv1 = new ImageView();
-        iv1.setFitWidth(80);
-        iv1.setFitHeight(100);
+        iv1.setFitWidth(240);
+        iv1.setFitHeight(300);
         iv1.setImage(avatar);
         GridPane.setConstraints(iv1, 0,0,1,3);
 
         String fontFamily1 = "Helvetica";
         double titleFontSize1 = 19;
 
-        Text text2 = new Text(10, 20, "Furkan Arif Bozdağ");
+        int i = 1;
+        Text text1 = new Text(10, 20, Main.arr.get(i).getUserName() + " " + Main.arr.get(i).getUserSurname());
+        text1.setFont(Font.font(fontFamily1, titleFontSize1));
+
+        Text text2 = new Text(10, 20, Main.arr.get(i).getUserInstitution());
         text2.setFont(Font.font(fontFamily1, titleFontSize1));
-        GridPane.setConstraints(text2, 1,0,2,1);
+        GridPane.setConstraints(text2, 0,1,2,1);
 
-        Text text3 = new Text(10, 20, "Bilkent University");
+        Text text3 = new Text(10, 20,  Main.arr.get(i).getUserDepartment()+ " / " + Main.arr.get(i).getUserTitle());
         text3.setFont(Font.font(fontFamily1, titleFontSize1));
-        GridPane.setConstraints(text3, 1,1,2,1);
+        GridPane.setConstraints(text3, 0,2);
 
-        Text text4 = new Text(10, 20, "CS /");
+        Text text4 = new Text(10, 20, Main.arr.get(i).getUserEmail());
         text4.setFont(Font.font(fontFamily1, titleFontSize1));
-        GridPane.setConstraints(text4, 1,2);
-
-        Text text5 = new Text(10, 20, "Undergraduate");
-        text5.setFont(Font.font(fontFamily1, titleFontSize1));
-        GridPane.setConstraints(text5, 2,2);
-
-        Text text6 = new Text(10, 20, "arif.bozdag@ug.bilkent.edu.tr");
-        text6.setFont(Font.font(fontFamily1, titleFontSize1));
-        GridPane.setConstraints(text6, 0,3,3,3);
+        GridPane.setConstraints(text4, 0,3,3,3);
 
         Hyperlink returnlink = new Hyperlink("return");
         returnlink.setOnAction(e -> {
-            LoginApp.outStage.setScene(HomePage.startScene());
-            LoginApp.outStage.setTitle("Home Page");
+            Main.outStage.setScene(HomePage.startScene());
+            Main.outStage.setTitle("Home Page");
         });
         GridPane.setConstraints(returnlink, 0,7);
 
-        infogrid.getChildren().addAll(iv1, text2, text3, text4, text5, text6, returnlink);
-        infogrid.setAlignment(Pos.CENTER);
+        infogrid.getChildren().addAll(text1, text2, text3, text4, returnlink);
 
+        layout.getChildren().addAll(iv1, infogrid);
 
-        scene = new Scene(infogrid);
+        scene = new Scene(layout);
     }
 
     public static Scene startScene() {
