656e770f57c0be6d5d14c38e7781a8546adda292
Rumeyza <rumeyzadincer@hotmail.com>
2017-04-21 14:26:08 +0300

    Some attributess are added

--- MasteringBilkent/src/User.java
+++ MasteringBilkent/src/User.java
@@ -1,4 +1,11 @@
 
 public class User {
+	private final String username = "";
+	private final String password = "";
+	private final String email = "";
+	private final String institution = "";
+	private final String title = "";
+	
+	
 
 }
