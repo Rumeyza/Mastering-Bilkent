2974ccb06d06e4d4953c0b8a9bb09f6711a6dc22
Rumeyza <rumeyzadincer@gmailcom>
2017-04-21 14:15:17 +0300

    Register Page Changed

--- MasteringBilkent/src/RegisterPage.java
+++ MasteringBilkent/src/RegisterPage.java
@@ -1,6 +1,5 @@
-
 import javafx.scene.Scene;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.GridPane;
 import javafx.geometry.Pos;
@@ -12,6 +11,9 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextFlow;
+import javafx.collections.*;
+import javafx.scene.control.ComboBox;
+ 
 
 public class RegisterPage {
 
@@ -20,68 +22,100 @@ public class RegisterPage {
     public static void start() {
 
         //Grid
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.TOP_LEFT);
-        grid.setHgap(100);
-        grid.setVgap(30);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-        grid.setStyle("-fx-background: #FFFFFF;");
+    	 GridPane grid = new GridPane();
+         grid.setAlignment(Pos.TOP_LEFT);
+         grid.setHgap(100);
+         grid.setVgap(30);
+         grid.setPadding(new Insets(25, 25, 25, 25));
+         grid.setStyle("-fx-background: #FFFFFF;");
 
         //TITLE
-        String fontFamily = "Helvetica";
-        double titleFontSize = 75;
-        TextFlow textFlow = new TextFlow();
-        Text scenetitle1 = new Text("MASTERING");
-        scenetitle1.setFill(Color.BLUE);
-        scenetitle1.setFont(Font.font(fontFamily, FontWeight.EXTRA_BOLD, titleFontSize));
-        Text scenetitle2 = new Text(" BILKENT");
-        scenetitle2.setFill(Color.RED);
-        scenetitle2.setFont(Font.font(fontFamily, FontWeight.EXTRA_BOLD, titleFontSize));
-        textFlow.getChildren().addAll(scenetitle1, scenetitle2);
-        grid.add(textFlow, 2, 0);
+         String fontFamily = "Helvetica";
+         double titleFontSize = 48;
 
+         TextFlow textFlow = new TextFlow();
+         
+         Text scenetitle1 = new Text("MASTERING");
+         scenetitle1.setFill(Color.BLUE);
+         scenetitle1.setFont(Font.font(fontFamily, FontWeight.EXTRA_BOLD, titleFontSize));
+         Text scenetitle2 = new Text(" BILKENT");
+         scenetitle2.setFill(Color.RED);
+         scenetitle2.setFont(Font.font(fontFamily, FontWeight.EXTRA_BOLD, titleFontSize));
+         
+         textFlow.getChildren().addAll(scenetitle1, scenetitle2);
+         
+         grid.add(textFlow, 2, 0);
         //user name
-        Label userName = new Label("Enter Your Name:");
-        grid.add(userName, 0, 2);
-        TextField userNameTextField = new TextField();
-        grid.add(userNameTextField, 1, 2);
+         Label userName = new Label("Enter Your Name:");
+         grid.add(userName, 0, 2);
+         TextField userNameTextField = new TextField();
+         grid.add(userNameTextField, 1, 2);
 
         //user surname
-        Label userSurname = new Label("Enter Your Surname:");
-        grid.add(userSurname, 0, 3);
-        TextField userSurnameTextField = new TextField();
-        grid.add(userSurnameTextField, 1, 3);
-
+         Label userSurname = new Label("Enter Your Surname:");
+         grid.add(userSurname, 0, 3);
+         TextField userSurnameTextField = new TextField();
+         grid.add(userSurnameTextField, 1, 3);
+         
         //user email
-        Label userEmail = new Label("Enter Your E-mail:");
-        grid.add(userEmail, 0, 4);
-        TextField userEmailTextField = new TextField();
-        grid.add(userEmailTextField, 1, 4);
+         Label userEmail = new Label("Enter Your E-mail:");
+         grid.add(userEmail, 0, 4);
+         TextField userEmailTextField = new TextField();
+         grid.add(userEmailTextField, 1, 4);
 
         //user choose password
-        Label pw = new Label("Enter Your Password:");
-        grid.add(pw, 0, 5);
-        PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1, 5);
+         
+         Label pw = new Label("Enter Your Password:");
+         grid.add(pw, 0, 5);
+         PasswordField pwBox = new PasswordField();
+         grid.add(pwBox, 1, 5);
 
         //user re enter password
-        Label repw = new Label("Re-Enter Your Password:");
-        grid.add(repw, 0, 6);
-        PasswordField repwBox = new PasswordField();
-        grid.add(repwBox, 1, 6);
+         Label repw = new Label("Re-Enter Your Password:");
+         grid.add(repw, 0, 6);
+         PasswordField repwBox = new PasswordField();
+         grid.add(repwBox, 1, 6);
 
         //user department
-        Label userDepartment = new Label("Institution:");
-        grid.add(userDepartment, 0, 7);
-        TextField userDepartmentTextField = new TextField();
-        grid.add(userDepartmentTextField, 1, 7);
+         Label userDepartment = new Label("Institution:");
+         grid.add(userDepartment, 0, 7);
+         TextField userDepartmentTextField = new TextField();
+         grid.add(userDepartmentTextField, 1, 7);
+         
 
-        Hyperlink returnlink = new Hyperlink("return");
-        returnlink.setOnAction(e -> {
-            LoginApp.outStage.setScene(LoginApp.startScene());
-            LoginApp.outStage.setTitle("Mastering Bilkent");
-        });
-        grid.add(returnlink,1,8);
+         Button registerButton = new Button("Complete Registration");
+         registerButton.setStyle("-fx-background-color: #00ffff");
+         
+         grid.add( registerButton, 2, 8);
+         
+         // User Roles:
+         ObservableList<String> userRoles = 
+         	    FXCollections.observableArrayList(
+         	        "a Student",
+         	        "an Instructor"
+         	    );
+         	final ComboBox<String> userRolesBox = new ComboBox<String>(userRoles);
+         	userRolesBox.setPromptText("I am");
+         	grid.add( userRolesBox, 2, 3);
+         	
+         	userRolesBox.setOnAction((event)->{
+         		String role = (String) userRolesBox.getSelectionModel().getSelectedItem();
+         		if( role == "a Student"){    	        	  
+         	        //User Status for Student:
+         	        ObservableList<String> studentStatus = 
+         	        	    FXCollections.observableArrayList( "Undergraduate", "Graduate", "None"   );
+         	        final ComboBox<String> studentStatusBox = new ComboBox<String>(studentStatus);
+         	        studentStatusBox.setPromptText( "My Ünvan is");
+         	        grid.add( studentStatusBox, 2, 4);
+         		}
+         		else{
+         	        ObservableList<String> instructorStatus = 
+         	        	    FXCollections.observableArrayList( "Prof.","Assoc. Prof", "Asst. Prof", "Instructor");
+         	        final ComboBox<String> instructorStatusBox = new ComboBox<String>(instructorStatus);
+         	        instructorStatusBox.setPromptText( "My Ünvan is");
+         	        grid.add( instructorStatusBox, 2, 4);
+         		}
+         	});
 
 
         scene = new Scene(grid);
