48b6969796f40463e5b75646e75def3b75f6f571
Rumeyza <rumeyzadincer@gmail.com>
2017-04-22 12:05:59 +0300

    main class syntax fixed

--- MasteringBilkent/src/Main.java
+++ MasteringBilkent/src/Main.java
@@ -7,8 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
     	 User user1 = new User();
-         User user2 = new User("Ertan", "Aday", "12345", "ertan.aday@ug.bilkent.edu.tr",
-                 "Bilkent University", "CS", "Undergraduate");
+         User user2 = new User("Ertan", "Aday", "12345", "ertan.aday@ug.bilkent.edu.tr","Bilkent University", "CS", "Undergraduate");
          arr = new ArrayList<User>();
          arr.add(0,user1);
          arr.add(1,user2);
