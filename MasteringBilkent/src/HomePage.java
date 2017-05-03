7741213ffef9b6cd93794252f18c24fa4eb1d285
Furkan Arif Bozdağ <bozdag.arif@gmail.com>
2017-05-03 20:04:34 +0300

    Update HomePage.java

--- MasteringBilkent/src/HomePage.java
+++ MasteringBilkent/src/HomePage.java
@@ -42,9 +42,16 @@ public class HomePage{
         //Line
         Line line = new Line(0, 20, 750, 20);
 
+        Hyperlink buttonB = new Hyperlink("Math 101");
+        buttonB.setOnAction(e-> {
+            LoginApp.myStage.setScene(CoursePage.startScene(userIndex));
+            LoginApp.myStage.setTitle("Mastering Bilkent - My Courses");
+        });
+
         Hyperlink buttonA = new Hyperlink("Register Another Course");
 
-        centerMenu.getChildren().addAll(logo,line, buttonA);
+        centerMenu.getChildren().addAll(logo, line, buttonB, buttonA);
+        
         //  CENTER END
 
         //  LEFT
