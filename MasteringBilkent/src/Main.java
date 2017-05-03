6a3a2c68d33c79e4bb5a12a9955cd9c7e35089fb
Furkan Arif Bozdağ <bozdag.arif@gmail.com>
2017-05-03 22:15:20 +0300

    Update Main.java

--- MasteringBilkent/src/Main.java
+++ MasteringBilkent/src/Main.java
@@ -3,20 +3,38 @@ import javafx.application.Application;
 import java.util.ArrayList;
 
 public class Main {
+    
     static ArrayList<User> arr;
-    public static int findUser(String userName, String password){
-    	int result = -1;
-    	for(int i = 0; i < Main.arr.size() ; i++){
-    		if(Main.arr.get(i).getUserEmail().equals(userName)&& Main.arr.get(i).getUserPassword().equals(password))
-    			result = i;
-    	}
-    	return result;
+    static ArrayList<Content> contArr;
+    
+    public static void main(String[] args) {
+        
+        User user1 = new User("Root","Mastering Bilkent","a","a","","","",""); //root@mastering.bilkent
+        arr = new ArrayList<User>();
+        arr.add(0,user1);
+        
+        Content OOD = new Course("Object Oriented Design - CS319" , "Bora Güngören");
+        contArr = new ArrayList<Content>();
+        contArr.add(0, OOD);
+        
+        Application.launch(LoginApp.class, args);
     }
 
-    public static void main(String[] args) {
-         User user1 = new User("Root","Mastering Bilkent","root","root@mastering.bilkent","","","","");
-         arr = new ArrayList<User>();
-         arr.add(0,user1);
-         Application.launch(LoginApp.class, args);
+    public static int findContent(int contId){
+        int result = -1;
+        for(int i = 0; i < Main.contArr.size() ; i++){
+            if(Main.contArr.get(i).contentId == contId)
+                result = i;
+        }
+        return result;
+    }
+    
+    public static int findUser(String userName, String password){
+        int result = -1;
+        for(int i = 0; i < Main.arr.size() ; i++){
+            if(Main.arr.get(i).getUserEmail().equals(userName)&& Main.arr.get(i).getUserPassword().equals(password))
+                result = i;
+        }
+        return result;
     }
 }
