2db53ba39e227131a09b6b4d1c1758cad6b416cf
Rumeyza <rumeyzadincer@gmail.com>
2017-05-05 15:55:11 +0300

    comment

--- MasteringBilkent/src/Offerings.java
+++ MasteringBilkent/src/Offerings.java
@@ -1,4 +1,17 @@
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
+import javafx.scene.shape.*;
 
 public class Offerings {
-
+	static Scene scene;
+	
+//comment
 }
