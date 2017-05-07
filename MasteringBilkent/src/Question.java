2b2924e86b934c8327b7c98a13fe4d86028d077f
Furkan Arif Bozdağ <bozdag.arif@gmail.com>
2017-05-07 23:10:36 +0300

    quiz and question classes

---
+++ MasteringBilkent/src/Question.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+/**
+ * Created by Asus on 7.5.2017.
+ */
+public class Question { //asdfbkhgbhjbkhkbh
+
+    private String questionText;
+    private String[] choiseList;
+    private String answer;
+
+    public Question(String questionText, String[] choiseList, String answer) {
+        this.questionText = questionText;
+        this.choiseList = choiseList;
+        this.answer = answer;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public String[] getChoiseList() {
+        return choiseList;
+    }
+
+    public void setChoiseList(String[] choiseList) {
+        this.choiseList = choiseList;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "questionText='" + questionText + '\'' +
+                ", choiseList=" + Arrays.toString(choiseList) +
+                ", answer='" + answer + '\'' +
+                '}';
+    }
+}
---
+++ MasteringBilkent/src/Quiz.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Asus on 7.5.2017.
+ */
+public class Quiz extends Content{
+
+    private int size;
+    private ArrayList<Question> questionsList;
+    private String description;
+    private boolean graded;
+
+    public Quiz() {
+        super( "Default Quiz");
+        this.size = 1;
+        this.description = "This is an example quiz of size of one question.";
+        this.graded = false;
+    }
+
+
+    public Quiz(String contentName, int size, String description, boolean graded) {
+        super(contentName);
+        this.size = size;
+        this.description = description;
+        this.graded = graded;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isGraded() {
+        return graded;
+    }
+
+    public void setGraded(boolean graded) {
+        this.graded = graded;
+    }
+
+    public void addQuestion(String questionText, String[] choiceList, String answer){
+        Question q = new Question(questionText, choiceList, answer);
+        questionsList.add(q);
+    }
+
+    public void deleteQuestion(int questionNum){
+        questionsList.remove(questionNum);
+    }
+
+    public void returnQuestion(int questionNum){
+        questionsList.get(questionNum);
+    }
+
+
+}
