2efca9d00c96b5415068fa2fdfd84c6296d4a3c5
Furkan Arif Bozdağ <bozdag.arif@gmail.com>
2017-05-08 01:56:00 +0300

    videopage class

---
+++ MasteringBilkent/src/VideoPage.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+/**
+ * Created by Asus on 8.5.2017.
+ */
+public class VideoPage {
+    public class Main extends Application {
+
+        public void main(String[] args) { launch(args); }
+
+        @Override
+        public void start(Stage stage) throws Exception {
+
+            WebView webview = new WebView();
+            webview.getEngine().load("https://www.google.com/");
+            webview.setPrefSize(1080, 720);
+
+            stage.setScene(new Scene(webview));
+            stage.show();
+        }
+    }
+}
