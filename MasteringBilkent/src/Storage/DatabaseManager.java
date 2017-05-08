3e2a4d8397665b0a440ece8cd6b29d22144ead69
Administrator <Administrator@139.179.224.73>
2017-05-08 18:40:05 +0300

    ılgın

--- MasteringBilkent/src/Storage/DatabaseManager.java
+++ MasteringBilkent/src/Storage/DatabaseManager.java
@@ -29,9 +29,7 @@ public class DatabaseManager {
 	public static void a(){
 		int a = 0;
 	}
-	public static void b(){
-		int a = 0;
-	}
+
 	//creations
 	public void createTableInstructor() throws Exception{
 		try{
